a8d8d3fb4a5d85e58ddf158abb0feeb8d98e3cef
Matan <matan.hadad1@mail.huji.ac.il>
2022-12-11 23:05:23 +0200

    updated jackAnalyzer

--- JackAnalyzer.py
+++ JackAnalyzer.py
@@ -15,15 +15,14 @@ from JackTokenizer import JackTokenizer
 def analyze_file(
         input_file: typing.TextIO, output_file: typing.TextIO) -> None:
     """Analyzes a single file.
-
     Args:
         input_file (typing.TextIO): the file to analyze.
         output_file (typing.TextIO): writes all output to this file.
     """
     # Your code goes here!
     # It might be good to start by creating a new JackTokenizer and CompilationEngine:
-    # tokenizer = JackTokenizer(input_file)
-    # engine = CompilationEngine(tokenizer, output_file)
+    tokenizer = JackTokenizer(input_file)
+    engine = CompilationEngine(tokenizer, output_file)
     pass
 
 
